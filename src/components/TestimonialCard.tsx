e343146632d467cc466a122ab9acf43bf9b07a80
jaysofty <jaysoft612@gmail.com>
2025-07-13 08:04:59 +0100

    final commit before break

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -13,7 +13,7 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
   author,
   company,
 }) => {
-  console.log("QUOTE", quote);
+  // console.log("QUOTE", quote);
   return (
     <div className="card border-0 shadow-sm p-3 h-100">
       <div className="d-flex flex-column flex-md-row align-items-center gap-3">
